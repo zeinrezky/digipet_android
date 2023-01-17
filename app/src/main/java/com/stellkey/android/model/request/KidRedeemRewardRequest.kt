4863f7bfd135f5a4c7c78918f3e1ce08e7c62fd1
nicolasmanurung <nicolas.manurung@dana.id>
2023-01-17 10:12:15 +0700

    feat(Kid Home Rewards): Implement redeem request

--- app/src/main/java/com/stellkey/android/model/request/KidRedeemRewardRequest.kt
+++ app/src/main/java/com/stellkey/android/model/request/KidRedeemRewardRequest.kt
@@ -1,6 +1,10 @@
 package com.stellkey.android.model.request
 
+import com.google.gson.annotations.SerializedName
+
 data class KidRedeemRewardRequest(
+    @SerializedName("globalRewardId")
     val globalRewardId: Int?,
+    @SerializedName("rewardId")
     val rewardId: Int?
 )
