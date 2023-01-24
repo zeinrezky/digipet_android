ca46170a8bfd2740a8c3d45591fef0e980ae141d
nicolasmanurung <nicolas.manurung@dana.id>
2023-01-24 22:20:34 +0700

    feat(Pet): add pet page

--- app/src/main/java/com/stellkey/android/view/child/pet/ChildPetFragment.kt
+++ app/src/main/java/com/stellkey/android/view/child/pet/ChildPetFragment.kt
@@ -4,17 +4,71 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.stellkey.android.R
+import com.stellkey.android.databinding.FragmentChildPetBinding
+import com.stellkey.android.helper.extension.textOrNull
+import com.stellkey.android.model.KidInfoModel
 import com.stellkey.android.view.base.BaseFragment
+import com.stellkey.android.view.child.ChildMainAct
+import com.stellkey.android.view.child.ChildViewModel
+import com.stellkey.android.view.intro.auth.LoginChooseProfileFragment
+import org.koin.android.ext.android.inject
 
 class ChildPetFragment : BaseFragment() {
+
+    private lateinit var dataBinding: FragmentChildPetBinding
+    private val viewModel by inject<ChildViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_child_pet, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_child_pet, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataBinding.vm = viewModel
+        dataBinding.lifecycleOwner = this
+
+        with(viewModel) {
+            isLoading.observe(viewLifecycleOwner) { isLoading ->
+                if (isLoading) {
+                    showWaitingDialog()
+                } else {
+                    hideWaitingDialog()
+                }
+            }
+
+            kidInfoResponse.observe(viewLifecycleOwner) {
+                it?.let { it1 -> setKidView(it1) }
+            }
+        }
+
+        setView()
+        setOnClick()
+    }
+
+    private fun setView() {
+        viewModel.getKidInfo()
+    }
+
+    private fun setKidView(data: KidInfoModel) {
+        dataBinding.apply {
+            tvChildLevel.textOrNull = data.level.level.toString()
+            piChildLevel.progress = 100 - data.level.percentageToNextLevel
+        }
+    }
+
+    private fun setOnClick() {
+        dataBinding.cvSwitchUser.setOnClickListener {
+            (activity as ChildMainAct).showMenu(isShow = false)
+            addFragment(LoginChooseProfileFragment.newInstance())
+        }
     }
 
     companion object {
